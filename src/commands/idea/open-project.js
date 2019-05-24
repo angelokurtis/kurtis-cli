6c8e96eb6f7baac0474d5126e3f6656186713ce6
Tiago Angelo <kurtis.angelo@gmail.com>
2019-05-24 20:35:57 -0300

    Open projects with Intellij IDEA

---
+++ src/commands/idea/open-project.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const cli = require('caporal');
+const handle = require('../../../middleware/output-handler');
+const sh = require('../../../middleware/bash');
+const path = require('path');
+
+async function openProject(args, {projectPath}) {
+    projectPath = projectPath || '.';
+    projectPath = `${path.resolve(projectPath)}/`;
+    try {
+        const command = `/snap/intellij-idea-ultimate/current/bin/idea.sh ${projectPath}`;
+        await sh(`nohup ${command} >/dev/null 2>&1 &`, true);
+    } catch (e) {
+        handle.error(e);
+    }
+}
+
+module.exports = function (command) {
+    cli
+        .command(command, '')
+        .option('-p, --project-path <project-path>', '', cli.STRING)
+        .action(openProject);
+};
